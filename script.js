a856486cead3f9e980ff040d212f3477c5570c08
dgasp24 <dommygabby7@gmail.com>
2026-04-15 19:17:26 -0500

    Refactor calculator layout and styles; enhance button functionality and display

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("hello")
+const container = document.querySelector(".buttonContainer");
+const mainText = document.querySelector("#answers")
+const history = document.querySelector("#textHistory");
+
+container.addEventListener("click", (e) =>{
+    if(!e.target.matches("button")) return;
+
+    const value = e.target.textContent;
+
+    if(value == "X"){value = "*"};
+
+    if(value === "AC"){
+        history.textContent = "";
+        mainText.textContent = ""
+        return;
+    }
+    if(value === "="){
+        history.textContent = `${mainText.textContent}`;
+        mainText.textContent = parseFloat(eval(mainText.textContent).toFixed(4));
+        return;
+    }
+    if(value === "<"){
+        mainText.textContent = mainText.textContent.slice(0,-1);
+        return;
+    }
+
+    mainText.textContent += value;
+});
