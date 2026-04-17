b6bdfff158c6494533e093590723919412cc2b78
dgasp24 <dommygabby7@gmail.com>
2026-04-16 23:32:37 -0500

    Add clear button functionality and improve input handling in calculator

--- script.js
+++ script.js
@@ -1,28 +1,59 @@
 const container = document.querySelector(".buttonContainer");
 const mainText = document.querySelector("#answers")
 const history = document.querySelector("#textHistory");
+const clear = document.querySelector("#clear");
 
 container.addEventListener("click", (e) =>{
     if(!e.target.matches("button")) return;
 
     const value = e.target.textContent;
+    if(mainText.textContent != ""){
+        clear.textContent = "C";
+    }
+    checkValue(value);
 
-    if(value == "X"){value = "*"};
+});
+
+function checkValue(value){
+      if(value == "X"){value = "*"};
 
     if(value === "AC"){
         history.textContent = "";
-        mainText.textContent = ""
+        mainText.textContent = "";
         return;
+    }else if(value === "C"){
+         history.textContent = "";
+         mainText.textContent = ""
+         clear.textContent = "AC";
+         return;
     }
     if(value === "="){
-        history.textContent = `${mainText.textContent}`;
-        mainText.textContent = parseFloat(eval(mainText.textContent).toFixed(4));
+        if("+/-*".includes(mainText.textContent.slice(-1))){
+            mainText.textContent = "ERROR";
+            return;
+        }if(mainText.textContent != "ERROR"){
+            history.textContent = `${mainText.textContent}`;
+            mainText.textContent = parseFloat(eval(mainText.textContent).toFixed(4));
+        }
         return;
     }
     if(value === "<"){
         mainText.textContent = mainText.textContent.slice(0,-1);
         return;
     }
+    if(value === "+/-"){
+        mainText.textContent = -1 * eval(mainText.textContent);
+        return;
+    }
 
+    if ("+/-*".includes(value)) {
+    // and last character is ALSO an operator → replace it
+    if ("+/-*".includes(mainText.textContent.slice(-1))) {
+        mainText.textContent = mainText.textContent.slice(0, -1) + value;
+        return;
+    }
+}
+
+    
     mainText.textContent += value;
-});
+}
